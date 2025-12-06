4e6ca0366784df1d0df54df92be1469881707437
Brian Perrett <perrettbrian@gmail.com>
2025-12-05 19:26:23 -0800

    update zsh files

---
+++ zsh/aliases.zsh
@@ -0,0 +1,8 @@
+alias ca="conda activate"
+alias cad="conda deactivate"
+
+alias gs="git status"
+alias gd="git diff"
+alias goh="cd ~"
+alias goconfig="cd ~/.config"
+alias rr="source ~/.zshrc"
--- zsh/base.zsh
+++ zsh/base.zsh
@@ -1,22 +1,6 @@
-export HISTFILE="$HOME/.zsh_history"
-export HISTSIZE=100000
-export SAVEHIST=$HISTSIZE
-setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
-setopt SHARE_HISTORY             # Share history between all sessions.
-setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
-setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
-setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
-setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
-setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
-setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
-setopt APPEND_HISTORY            # append to history file
-setopt HIST_NO_STORE             # Don't store history commands
-# Cycle through history based on characters already typed on the line
-autoload -U up-line-or-beginning-search
-autoload -U down-line-or-beginning-search
-zle -N up-line-or-beginning-search
-zle -N down-line-or-beginning-search
-bindkey "$key[Up]" up-line-or-beginning-search
-bindkey "$key[Down]" down-line-or-beginning-search
+# history
+[ -f ~/.config/dotfiles/zsh/history.zsh ] && source ~/.config/dotfiles/zsh/history.zsh
+
+[ -f ~/.config/dotfiles/zsh/aliases.zsh ] && source ~/.config/dotfiles/zsh/aliases.zsh
+
+export HOMEBREW_NO_AUTO_UPDATE=1
