f3c9018e195ca362cb342b8c18726f1382f522d4
brian perrett <perrettbrian@gmail.com>
2026-03-05 19:10:13 -0800

    nvim-rg function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,3 +7,8 @@ alias gb="git branch"
 alias goh="cd ~"
 alias goconfig="cd ~/.config"
 alias rr="source ~/.zshrc"
+
+# use ripgrep and pipe its output into nvim
+nvim-rg() {
+    rg --vimgrep "$@" | nvim -q - -c "copen"
+}
